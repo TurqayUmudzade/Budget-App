8687a61c7d08c98b68c32100f31337429b51840e
turgayau@code.edu.az <turgayau@code.edu.az>
2019-12-14 22:50:10 +0400

    chart added

--- dashboard.php
+++ dashboard.php
@@ -16,6 +16,8 @@
     <!-- ICON Monster -->
     <link rel="stylesheet" href="https://cdn.iconmonstr.com/1.3.0/css/iconmonstr-iconic-font.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+         <!-- Chart -->
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
     <!-- MY CSS -->
     <link rel="stylesheet" href="style.css">
 
@@ -310,7 +312,81 @@
                 </div>
             </div>
         </section>
+      </div>
+
+    
+
+   <section id="chart"> 
+       <div class="container">
+        <canvas id="myChart"></canvas>
     </div>
+</section>
+
+<script>
+let myChart = document.getElementById('myChart').getContext('2d');
+
+// Global Options
+Chart.defaults.global.defaultFontFamily = 'Lato';
+Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontColor = '#777';
+
+let massPopChart = new Chart(myChart, {
+    type: 'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+    data: {
+        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets: [{
+            label: 'Population',
+            data: [
+                617594,
+                181045,
+                153060,
+                106519,
+                105162,
+                95072
+            ],
+            //backgroundColor:'green',
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(54, 162, 235, 0.6)',
+                'rgba(255, 206, 86, 0.6)',
+                'rgba(75, 192, 192, 0.6)',
+                'rgba(153, 102, 255, 0.6)',
+                'rgba(255, 159, 64, 0.6)',
+                'rgba(255, 99, 132, 0.6)'
+            ],
+            borderWidth: 1,
+            borderColor: '#777',
+            hoverBorderWidth: 3,
+            hoverBorderColor: '#000'
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Largest Cities In Massachusetts',
+            fontSize: 25
+        },
+        legend: {
+            display: true,
+            position: 'right',
+            labels: {
+                fontColor: '#000'
+            }
+        },
+        layout: {
+            padding: {
+                left: 50,
+                right: 0,
+                bottom: 0,
+                top: 0
+            }
+        },
+        tooltips: {
+            enabled: true
+        }
+    }
+});</script>
+   
 
     <footer>
         <div class="container">
@@ -394,6 +470,7 @@
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="Loader.js"></script>
+
 
 </body>
 
